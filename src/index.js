6e1006badfdb71342993f3af99bdb3fa6d36d54b
cdanni91 <cdanni91@gmail.com>
2025-07-31 13:01:17 -0300

    Add fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+
+
+// test element
+// const body = document.querySelector("body");
+// const element = document.createElement("div")
+
+// element.innerText = "Hola"
+
+// body.appendChild(element);
+
+const location = "London,UK"
+const api_key = "6NCEX7UPQA4T8WRSB8UEVKX3A"
+
+const baseURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${api_key}`
+
+
+const req = fetch(baseURL, {
+    mode: "cors"
+})
+.then(response => response.json())
+.then(data => console.log(data));
+
