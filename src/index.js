d1255335e1a2c5c4ee8edfc9c5a49a526dbeb7de
cdanni91 <cdanni91@gmail.com>
2025-08-01 12:52:17 -0300

    Add the functions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,66 @@
 import "./styles.css";
 
 
-// test element
-// const body = document.querySelector("body");
-// const element = document.createElement("div")
+const bodyElement = document.querySelector("body");
+const buttonGenerator = document.createElement("button");
+buttonGenerator.innerText = "Activar funciones"
+buttonGenerator.addEventListener("click", main);
 
-// element.innerText = "Hola"
-
-// body.appendChild(element);
-
-const location = "London,UK"
-const api_key = "6NCEX7UPQA4T8WRSB8UEVKX3A"
-
-const baseURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${api_key}`
+bodyElement.appendChild(buttonGenerator);
 
 
-const req = fetch(baseURL, {
-    mode: "cors"
-})
-.then(response => response.json())
-.then(data => console.log(data));
+function main() {
+
+    let data = {}
+
+
+    const form = document.querySelector(".weather-form");
+    const formButton = document.querySelector(".form-submit")
+
+    form.addEventListener("submit", modifySubmit);
+
+
+
+    function modifySubmit(event) {
+
+        event.preventDefault();
+        
+        const formData = new FormData(form, formButton);
+        const dataObject = Object.fromEntries(formData.entries())
+
+        data = dataObject
+
+        makeRequest(data);
+
+    }
+
+
+    async function makeRequest(data) {
+
+        const api_key = "6NCEX7UPQA4T8WRSB8UEVKX3A"
+        const cityName = data['city-name']
+        const countryName = data['country-name']
+        const location = `${cityName}`+","+`${countryName}`
+        const baseURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${api_key}`
+
+        try {
+
+            const request = await fetch(baseURL, {
+                mode: "cors"
+            })
+
+            const response = await request.json()
+
+            console.log(response);
+
+        }
+
+        catch {
+            console.log("woops")
+
+        }
+
+    }
+
+}
 
